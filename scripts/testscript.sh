81c4c340a67770e788fb21cbd6b8eaa18a12f8cd
Kaixi26 <jorgejg575926@gmail.com>
2019-05-13 08:33:21 +0100

    _

--- scripts/testscript.sh
+++
@@ -1,8 +0,0 @@
-(sh scripts/addstock.sh 10 1 100000 | ./cv &
- sh scripts/addstock.sh 10 2 500000 | ./cv &
- sh scripts/addstock.sh 10 3 800000 | ./cv &
- sh scripts/addstock.sh 1 1 1 | ./cv);
-
- (sh scripts/addstock.sh 10 1 -100000 | ./cv &
- sh scripts/addstock.sh  10 2 -500000 | ./cv &
- sh scripts/addstock.sh  10 3 -800000 | ./cv) &
